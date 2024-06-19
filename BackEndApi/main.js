b6037c1c46f3f33e386d20025ad68b656f27dcc4
Khursihed <Khursihed.khurishedabbas786@gmail.com>
2024-06-19 21:29:12 +0530

    Updated MeanStack

--- BackEndApi/main.js
+++ BackEndApi/main.js
@@ -67,56 +67,6 @@ app.get('/api/GetByDate/:dueDate', async(req, res)=>{
     res.status(500).json({error:'Internal Server Error'})
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.post('/api/createTask', async(req, res)=>{
-//     const {title,description,dueDate,priority} = req.body;
-//     const task = new Task({title,description,dueDate,priority });
-//     await task.save();
-//     res.status(201).json(({message:'Task Created Successfully'}))
-// })
-
-// app.get('/api/GetTask/:dueDate', async(req, res)=>{
-//  try{
-//   const  {dueDate} = req.params;
-//   if(!dueDate){
-//     return res.status(400).json({error:'dueDate is Required'});
-
-//   }
-//   const parsedDueDate = moment.utc(dueDate, 'YYYY-MM-DD').startOf('day').toDate();
-//   console.log('Parsed dueDate:', parsedDueDate);
-//   const task = await Task.find({ dueDate: new Date(parsedDueDate) });
-//   console.log('Database query:', { dueDate: parsedDueDate });
-
-//   if (!task) {
-//     return res.status(404).json({ error: 'Task not found' });
-//   }
-  
-//   res.status(200).json([task]);
-// }catch (error) {
-//   console.error(error);
-//   res.status(500).json({ error: 'Internal server error' });
-// }
-// })
-
 // Update Task Route
 app.put('/api/UpdateTask/:id', async (req, res) => {
   try {
